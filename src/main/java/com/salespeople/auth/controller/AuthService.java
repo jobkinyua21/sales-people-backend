9f83f417b187811aacd8c2a1501608c2d1312568
jobu <jobkinyua21@gmail.com>
2026-04-14 22:54:49 +0300

    first commit

--- src/main/java/com/salespeople/auth/controller/AuthService.java
+++ src/main/java/com/salespeople/auth/controller/AuthService.java
@@ -68,6 +68,8 @@ public class AuthService {
 
         validateUserStatus(user.getUsrStatus(), user.getIsActive());
 
+
+
         // Validate password
         if (!passwordEncoder.matches(request.getPassword(), user.getUsrPassword())) {
             loginAttemptService.incrementFailedAttempts(user);
